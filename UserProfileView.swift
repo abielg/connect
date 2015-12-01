28ace8910213f9594c99eeb1a511825d19de3408
Abiel Gutierrez <abielgutierrez95@gmail.com>
2015-12-01 02:10:00 -0800

    significant progress

--- UserProfileView.swift
+++ UserProfileView.swift
@@ -9,5 +9,9 @@
 import UIKit
 
 class UserProfileView: UIView {
+    
+    
+    
+    
 
 }
---
+++ UserProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileViewController.swift
+//  Connect
+//
+//  Created by Abiel Gutierrez on 12/1/15.
+//  Copyright © 2015 Abiel Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+    
+    @IBAction func changePicture(sender: AnyObject) {
+        var actionSheet =
+    }
+    
+
+}
