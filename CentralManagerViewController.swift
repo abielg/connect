22a863d7f1fca6ca5aa6c4da5abd15f73d032406
Abiel Gutierrez <abielgutierrez95@gmail.com>
2015-12-06 19:10:09 -0800

    Finished first implementation of bluetooth -- about to fix layout constraints bug

--- CentralManagerViewController.swift
+++ CentralManagerViewController.swift
@@ -8,17 +8,31 @@
 
 import UIKit
 import CoreBluetooth
+import Parse
 
 class CentralManagerViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     
-    var data = NSMutableData()
     var discoveredPeripheral: CBPeripheral?
     var centralManager = CBCentralManager()
+    
+    let SERVICE_UUID = "FB694B90-F49E-4597-8306-171BBA78F846"
+    let CHARACTERISTIC_UUID = "EB6727C4-F184-497A-A656-76B0CDAC633A"
+    
+    let user = PFUser.currentUser()
+    var newConnection: String?{
+        didSet{
+            //Make all the Parse connection shit
+        }
+    }
     
     override func viewDidLoad(){
         super.viewDidLoad()
         self.navigationItem.title = "Seek Connection"
         centralManager.delegate = self
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        centralManager.stopScan()
     }
     
     func centralManagerDidUpdateState(central: CBCentralManager) {
@@ -38,7 +52,7 @@ class CentralManagerViewController: UIViewController, CBCentralManagerDelegate, 
             presentViewController(alert, animated: true, completion: nil)
         case .PoweredOn:
             print("on")
-            centralManager.scanForPeripheralsWithServices([CBUUID(string: "FB694B90-F49E-4597-8306-171BBA78F846")], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+            centralManager.scanForPeripheralsWithServices([CBUUID(string: SERVICE_UUID)], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
         }
     }
     
@@ -47,9 +61,60 @@ class CentralManagerViewController: UIViewController, CBCentralManagerDelegate, 
         centralManager.connectPeripheral(peripheral, options: nil)
     }
     
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        centralManager.stopScan()
+        peripheral.delegate = self
+        if let service = peripheral.services?.first{
+            peripheral.discoverCharacteristics([CBUUID(string: CHARACTERISTIC_UUID)], forService: service)
+        }
+    }
+    
     func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        removeConnections()
         let alert = UIAlertController.createAlert("Error", withMessage: "Failed to connect to device.")
         presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        if error != nil{
+            print(error?.description)
+            return
+        }
+        //might want to check for the correct UUID
+        if let char = service.characteristics?.first{
+            peripheral.setNotifyValue(true, forCharacteristic: char)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        if error != nil {
+            print(error?.description)
+            return
+        }
+        
+        newConnection = String(data: characteristic.value!, encoding: NSUTF8StringEncoding)
+        peripheral.setNotifyValue(false, forCharacteristic: characteristic)
+        centralManager.cancelPeripheralConnection(peripheral)
+        let alert = UIAlertController.createAlert("Connected to \(newConnection)")
+        presentViewController(alert, animated: true, completion: nil)
+
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateNotificationStateForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        if !characteristic.isNotifying{
+            centralManager.cancelPeripheralConnection(peripheral)
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        discoveredPeripheral = nil
+        print("connection lost")
+    }
+    
+    func removeConnections(){
+        if let peri = discoveredPeripheral{
+            centralManager.cancelPeripheralConnection(peri)
+        }
     }
     
 }
--- PeripheralViewController.swift
+++ PeripheralViewController.swift
@@ -6,7 +6,38 @@
 //  Copyright © 2015 Abiel Gutierrez. All rights reserved.
 //
 import UIKit
+import CoreBluetooth
+import Parse
 
-class PeripheralViewController: UIViewController {
-
+class PeripheralViewController: UIViewController, CBPeripheralManagerDelegate {
+    let peripheralManager = CBPeripheralManager()
+    var characteristic: CBMutableCharacteristic?
+    var sentData = NSData()
+    let dataIndex = NSInteger()
+    
+    let SERVICE_UUID = "FB694B90-F49E-4597-8306-171BBA78F846"
+    let CHARACTERISTIC_UUID = "EB6727C4-F184-497A-A656-76B0CDAC633A"
+    
+    let user = PFUser.currentUser()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey:SERVICE_UUID])
+        sentData = ("Alberto").dataUsingEncoding(NSUTF8StringEncoding)!
+    }
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
+        if peripheral.state != CBPeripheralManagerState.PoweredOn{
+            print("SUPPORT BLUETOOTH")
+            return
+        }
+        
+        if peripheral.state != CBPeripheralManagerState.PoweredOn{
+            characteristic = CBMutableCharacteristic(type: CBUUID(string: CHARACTERISTIC_UUID), properties: CBCharacteristicProperties.Notify, value: sentData, permissions: CBAttributePermissions.Readable)
+            let service = CBMutableService(type: CBUUID(string: SERVICE_UUID), primary: true)
+            service.characteristics = [characteristic!]
+            peripheralManager.addService(service)
+            print("service added")
+        }
+    }
 }
